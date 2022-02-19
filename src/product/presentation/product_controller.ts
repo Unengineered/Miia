72b14420763ad85f9a23579f45d2ebf465518133
advait <advaitbansode4@gmail.com>
2022-02-19 17:13:53 +0530

    FEATURE: wrote tests for product controller

--- src/product/presentation/product_controller.ts
+++ src/product/presentation/product_controller.ts
@@ -1,4 +1,4 @@
-import {InterServiceMessage, SendTo} from "../../core/models/inter_service_message";
+import { InterServiceMessage, SendTo } from "../../core/models/inter_service_message";
 import WebsocketMessage from "../../core/models/websocket_message";
 import WebsocketRequest from "../../core/models/websocket_request";
 import ProductError from "../domain/errors/product_error";
@@ -6,56 +6,56 @@ import Url from 'url-parse'
 import ProductService from "../application/product_service";
 import WebsocketResponse from "../../core/models/websocket_response";
 
-export default class ProductController{
+export default class ProductController {
 
     readonly productService: ProductService
 
-    constructor({productService} : {productService: ProductService}){
+    constructor({ productService }: { productService: ProductService }) {
         this.productService = productService
     }
 
-    async handleRequest(interServiceMessage: InterServiceMessage): Promise<InterServiceMessage[]>{
-        if(interServiceMessage.packet instanceof WebsocketRequest){
-            switch(interServiceMessage.packet.method){
-                case "GET" : {
+    async handleRequest(interServiceMessage: InterServiceMessage): Promise<InterServiceMessage[]> {
+        if (interServiceMessage.packet instanceof WebsocketRequest) {
+            switch (interServiceMessage.packet.method) {
+                case "GET": {
                     return this.handleGetRequest(interServiceMessage)
                 }
-    
-                case "PUT" : {
+
+                case "PUT": {
                     return this.handlePutRequest(interServiceMessage)
                 }
-    
-                case "DELETE" : {
+
+                case "DELETE": {
                     return this.handleDeleteRequest(interServiceMessage)
                 }
             }
         }
-        throw new ProductError({code: "unimplimented"})
+        throw new ProductError({ code: "unimplimented" })
     }
 
-    async handleMessage(interServiceMessage: InterServiceMessage): Promise<InterServiceMessage[]>{
-        throw new ProductError({code: "unimplimented"})
-    } 
+    async handleMessage(interServiceMessage: InterServiceMessage): Promise<InterServiceMessage[]> {
+        throw new ProductError({ code: "unimplimented" })
+    }
 
-    private async handleGetRequest(interServiceMessage: InterServiceMessage) : Promise<InterServiceMessage[]>{
-        if(interServiceMessage.packet instanceof WebsocketRequest){
+    private async handleGetRequest(interServiceMessage: InterServiceMessage): Promise<InterServiceMessage[]> {
+        if (interServiceMessage.packet instanceof WebsocketRequest) {
             const url = new Url(interServiceMessage.packet.url)
-            switch(url.pathname){
-                case '/summary' : {
+            switch (url.pathname) {
+                case '/summary': {
                     const serviceResults = await this.productService.getSummaryProducts(interServiceMessage.packet)
                     return serviceResults.map((result) => {
-                            return new InterServiceMessage(
-                                {
-                                    packet: result,
-                                    socketId: interServiceMessage.socketId,
-                                    uid: interServiceMessage.uid,
-                                    sendTo: result instanceof WebsocketResponse ? SendTo.SOCKET_ID : SendTo.UID
-                                }
-                            )
+                        return new InterServiceMessage(
+                            {
+                                packet: result,
+                                socketId: interServiceMessage.socketId,
+                                uid: interServiceMessage.uid,
+                                sendTo: result instanceof WebsocketResponse ? SendTo.SOCKET_ID : SendTo.UID
+                            }
+                        )
                     })
                 }
-    
-                case '/detailed' : {
+
+                case '/detailed': {
                     const serviceResults = await this.productService.getDetailedProduct(interServiceMessage.packet)
 
                     return serviceResults.map((serviceResult) => {
@@ -63,21 +63,30 @@ export default class ProductController{
                             packet: serviceResult,
                             uid: interServiceMessage.uid,
                             socketId: interServiceMessage.socketId,
-                            sendTo: serviceResult instanceof WebsocketRequest ? SendTo.SOCKET_ID : SendTo.UID
+                            sendTo: serviceResult instanceof WebsocketResponse ? SendTo.SOCKET_ID : SendTo.UID
                         })
                     })
                 }
             }
         }
-        throw new ProductError({code: "unimplimented"})
+        throw new ProductError({ code: "unimplimented" })
     }
 
-    private async handlePutRequest(interServiceMessage: InterServiceMessage) : Promise<InterServiceMessage[]>{
-        const result = await this.productService.putProduct(interServiceMessage.packet as WebsocketRequest)
-        throw new ProductError({code: "unimplimented"})
+    private async handlePutRequest(interServiceMessage: InterServiceMessage): Promise<InterServiceMessage[]> {
+        const serviceResults = await this.productService.putProduct(interServiceMessage.packet as WebsocketRequest)
+        return serviceResults.map((result) => {
+            return new InterServiceMessage(
+                {
+                    packet: result,
+                    socketId: interServiceMessage.socketId,
+                    uid: interServiceMessage.uid,
+                    sendTo: result instanceof WebsocketResponse ? SendTo.SOCKET_ID : SendTo.UID
+                }
+            )
+        })
     }
 
-    private async handleDeleteRequest(interServiceMessage: InterServiceMessage) : Promise<InterServiceMessage[]>{
-        throw new ProductError({code: "unimplimented"})
+    private async handleDeleteRequest(interServiceMessage: InterServiceMessage): Promise<InterServiceMessage[]> {
+        throw new ProductError({ code: "unimplimented" })
     }
 }
--- test/product/presentation/product_controller.test.ts
+++ test/product/presentation/product_controller.test.ts
@@ -6,24 +6,31 @@ import WebsocketResponse from '../../../src/core/models/websocket_response'
 import ProductService from '../../../src/product/application/product_service'
 import ProductController from '../../../src/product/presentation/product_controller'
 
-
+/**
+ * These tests currently only test if the controller
+ * is calling the right methods of the service.
+ * 
+ * Ideally it should also test for the fact the controller
+ * returns whatever the service gives it without manipulation.
+ * This hasn't been done yet to save time.
+ */
 describe("PRODUCT CONTROLLER", function () {
 
     var productController: ProductController
     var productService = Sinon.createStubInstance(ProductService)
 
-    before(function(){
-        productController = new ProductController({productService: productService})
+    before(function () {
+        productController = new ProductController({ productService: productService })
     })
 
-    this.afterEach(function(){
+    this.afterEach(function () {
         productService.getDetailedProduct.reset()
         productService.getProductByStore.reset()
         productService.getSummaryProducts.reset()
         productService.putProduct.reset()
     })
 
-    it("should handle successful GET summary request", async function(){
+    it("should handle successful GET summary request", async function () {
         productService.getSummaryProducts.resolves(
             [
                 new WebsocketResponse({
@@ -32,7 +39,7 @@ describe("PRODUCT CONTROLLER", function () {
                     statusMessage: "OK",
                     headers: {},
                     body: {
-                        "summary_thrift_products" : [
+                        "summary_thrift_products": [
                             {
                                 id: 1,
                                 name: "FIRST PRODUCT",
@@ -64,7 +71,7 @@ describe("PRODUCT CONTROLLER", function () {
                 socketId: "SOCKET_ID"
             }
         ))
-        
+
         assert.deepEqual(response, [new InterServiceMessage(
             {
                 packet: new WebsocketResponse(
@@ -78,7 +85,7 @@ describe("PRODUCT CONTROLLER", function () {
                                 {
                                     id: 1,
                                     name: "FIRST PRODUCT",
-                                    thumbnail: "url1" 
+                                    thumbnail: "url1"
                                 },
                                 {
                                     id: 2,
@@ -93,10 +100,149 @@ describe("PRODUCT CONTROLLER", function () {
                 socketId: "SOCKET_ID",
                 sendTo: SendTo.SOCKET_ID
             }
-        )] )
+        )])
     })
 
-    it("should handle PUT request")
+    it("should handle GET detailed product request", async function () {
+        productService.getDetailedProduct.resolves([
+            new WebsocketResponse({
+                responseId: "ID",
+                statusCode: 200,
+                statusMessage: "OK",
+                headers: {},
+                body: {
+                    "id": 1,
+                    "name": "TEST_DOC",
+                    "price": 500,
+                    "originalPrice": 4000,
+                    "pictures": ["url1", "url2"],
+                    "sizeChart": [
+                        { "key": "chest", "value": "32" },
+                        { "key": "chest", "value": "32" }
+                    ]
+                }
+            })
+        ])
+
+        const response = await productController.handleRequest(new InterServiceMessage(
+            {
+                packet: new WebsocketRequest(
+                    {
+                        requestId: "ID",
+                        method: "GET",
+                        url: "https://www.everythng.in/detailed?id=2",
+                        headers: {},
+                        body: {}
+                    }
+                ),
+                uid: "UID",
+                socketId: "SOCKET_ID"
+            }
+        ))
+
+        assert.deepEqual(response, [
+            new InterServiceMessage(
+                {
+                    uid: "UID",
+                    socketId: "SOCKET_ID",
+                    sendTo: SendTo.SOCKET_ID,
+                    packet: new WebsocketResponse({
+                        responseId: "ID",
+                        statusCode: 200,
+                        statusMessage: "OK",
+                        headers: {},
+                        body: {
+                            "id": 1,
+                            "name": "TEST_DOC",
+                            "price": 500,
+                            "originalPrice": 4000,
+                            "pictures": ["url1", "url2"],
+                            "sizeChart": [
+                                { "key": "chest", "value": "32" },
+                                { "key": "chest", "value": "32" }
+                            ]
+                        }
+                    })
+                }
+            )
+        ])
+    })
+
+    it("should handle PUT request", async function () {
+        productService.putProduct.resolves([
+            new WebsocketResponse({
+                responseId: "ID",
+                statusCode: 200,
+                statusMessage: "OK",
+                headers: {},
+                body: {
+                    "id": 1,
+                    "name": "TEST_DOC",
+                    "price": 500,
+                    "originalPrice": 4000,
+                    "pictures": ["url1", "url2"],
+                    "sizeChart": [
+                        { "key": "chest", "value": "32" },
+                        { "key": "chest", "value": "32" }
+                    ]
+                }
+            })
+        ])
+
+        const results = await productController.handleRequest(
+            new InterServiceMessage(
+                {
+                    packet: new WebsocketRequest({
+                        requestId: "ID",
+                        method: "PUT",
+                        url: "https://www.everythng.com/product",
+                        headers: {},
+                        body: {
+                            "name": "TEST_DOC",
+                            "price": 500,
+                            "originalPrice": 4000,
+                            "pictures": ["url1", "url2"],
+                            "sizeChart": [
+                                { "key": "chest", "value": "32" },
+                                { "key": "chest", "value": "32" }
+                            ]
+                        }
+                    }),
+                    uid: "UID",
+                    socketId: "SOCKET_ID"
+                }
+            )
+        )
+
+        assert.deepEqual(results, [
+            new InterServiceMessage(
+                {
+                    uid: "UID",
+                    socketId: "SOCKET_ID",
+                    sendTo: SendTo.SOCKET_ID,
+                    packet: new WebsocketResponse(
+                        {
+                            responseId: "ID",
+                            statusCode: 200,
+                            statusMessage: "OK",
+                            headers: {},
+                            body: {
+                                "id": 1,
+                                "name": "TEST_DOC",
+                                "price": 500,
+                                "originalPrice": 4000,
+                                "pictures": ["url1", "url2"],
+                                "sizeChart": [
+                                    { "key": "chest", "value": "32" },
+                                    { "key": "chest", "value": "32" }
+                                ]
+                            }
+                        }
+                    )
+                }
+            )
+        ])
+    })
     it("should handle DELETE request")
 
 })
