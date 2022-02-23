0555b5ec51e2fce9da5b01f609c66d4ccc5d6e54
Ishan <ishan.kulkarni@somaiya.edu>
2022-02-24 00:29:30 +0530

    FIX: Save product(service) tests done! :bug:

--- test/product/application/product_service.test.ts
+++ test/product/application/product_service.test.ts
@@ -152,237 +152,55 @@ describe("PRODUCT SERVICE", function () {
 
     // })
     it("should save a product", async function () {
+        productRepository.saveProduct.resolves(
+            new DetailedThriftProductEntity(
+                {
+                    id: "122",
+                    name: "NAME",
+                    price: 200,
+                    originalPrice: 5000,
+                    pictures: ["url1", "url2"],
+                    sizeChart: [
+                        { key: "chest", value: "32" }
+                    ],
+                    storeLink: "122",
+                }
+            )
+        )
 
+
+        const response = await productService.putProduct(
+            new WebsocketRequest(
+                {
+                    requestId: "62148dbacd944133da8d8ad4",
+                    method: "GET",
+                    url: "https://www.everythng.in/detailed",
+                    headers: "" as any,
+                    body: "" as any,
+                }
+            )
+        )
+
+        assert.deepEqual(response, [
+            new WebsocketResponse({
+                responseId: "62148dbacd944133da8d8ad4",
+                statusCode: 200,
+                statusMessage: "OK",
+                headers: {},
+                body: new DetailedThriftProductEntity(
+                    {
+                        id: "122",
+                        name: "NAME",
+                        price: 200,
+                        originalPrice: 5000,
+                        pictures: ["url1", "url2"],
+                        sizeChart: [
+                            { key: "chest", value: "32" }
+                        ],
+                        storeLink: "122",
+                    }
+                ).toJson()
+            })
+        ])
     })
 })
-
-
-
-//TODO:(vd)
-
-/**
- * These tests currently only test if the controller
- * is calling the right methods of the service.
- *
- * Ideally it should also test for the fact the controller
- * returns whatever the service gives it without manipulation.
- * This hasn't been done yet to save time.
- */
-// describe("PRODUCT CONTROLLER", function () {
-
-
-//     it("should handle successful GET summary request", async function () {
-//         productService.getSummaryProducts.resolves(
-//             [
-//                 new WebsocketResponse({
-//                     responseId: "ID",
-//                     statusCode: 200,
-//                     statusMessage: "OK",
-//                     headers: {},
-//                     body: {
-//                         "summary_thrift_products": [
-//                             {
-//                                 id: 1,
-//                                 name: "FIRST PRODUCT",
-//                                 thumbnail: "url1"
-//                             },
-//                             {
-//                                 id: 2,
-//                                 name: "SECOND PRODUCT",
-//                                 thumbnail: "url2"
-//                             }
-//                         ]
-//                     }
-//                 })
-//             ]
-//         )
-
-//         const response = await productController.handleRequest(new InterServiceMessage(
-//             {
-//                 packet: new WebsocketRequest(
-//                     {
-//                         requestId: "ID",
-//                         method: "GET",
-//                         url: "https://www.everythng.in/summary",
-//                         headers: {},
-//                         body: {}
-//                     }
-//                 ),
-//                 uid: "UID",
-//                 socketId: "SOCKET_ID"
-//             }
-//         ))
-
-//         assert.deepEqual(response, [new InterServiceMessage(
-//             {
-//                 packet: new WebsocketResponse(
-//                     {
-//                         responseId: "ID",
-//                         statusCode: 200,
-//                         statusMessage: "OK",
-//                         headers: {},
-//                         body: {
-//                             "summary_thrift_products": [
-//                                 {
-//                                     id: 1,
-//                                     name: "FIRST PRODUCT",
-//                                     thumbnail: "url1"
-//                                 },
-//                                 {
-//                                     id: 2,
-//                                     name: "SECOND PRODUCT",
-//                                     thumbnail: "url2"
-//                                 }
-//                             ]
-//                         }
-//                     }
-//                 ),
-//                 uid: "UID",
-//                 socketId: "SOCKET_ID",
-//                 sendTo: SendTo.SOCKET_ID
-//             }
-//         )])
-//     })
-
-//     it("should handle GET detailed product request", async function () {
-//         productService.getDetailedProduct.resolves([
-//             new WebsocketResponse({
-//                 responseId: "ID",
-//                 statusCode: 200,
-//                 statusMessage: "OK",
-//                 headers: {},
-//                 body: {
-//                     "id": 1,
-//                     "name": "TEST_DOC",
-//                     "price": 500,
-//                     "originalPrice": 4000,
-//                     "pictures": ["url1", "url2"],
-//                     "sizeChart": [
-//                         { "key": "chest", "value": "32" },
-//                         { "key": "chest", "value": "32" }
-//                     ]
-//                 }
-//             })
-//         ])
-
-//         const response = await productController.handleRequest(new InterServiceMessage(
-//             {
-//                 packet: new WebsocketRequest(
-//                     {
-//                         requestId: "ID",
-//                         method: "GET",
-//                         url: "https://www.everythng.in/detailed?id=2",
-//                         headers: {},
-//                         body: {}
-//                     }
-//                 ),
-//                 uid: "UID",
-//                 socketId: "SOCKET_ID"
-//             }
-//         ))
-
-//         assert.deepEqual(response, [
-//             new InterServiceMessage(
-//                 {
-//                     uid: "UID",
-//                     socketId: "SOCKET_ID",
-//                     sendTo: SendTo.SOCKET_ID,
-//                     packet: new WebsocketResponse({
-//                         responseId: "ID",
-//                         statusCode: 200,
-//                         statusMessage: "OK",
-//                         headers: {},
-//                         body: {
-//                             "id": 1,
-//                             "name": "TEST_DOC",
-//                             "price": 500,
-//                             "originalPrice": 4000,
-//                             "pictures": ["url1", "url2"],
-//                             "sizeChart": [
-//                                 { "key": "chest", "value": "32" },
-//                                 { "key": "chest", "value": "32" }
-//                             ]
-//                         }
-//                     })
-//                 }
-//             )
-//         ])
-//     })
-
-//     it("should handle PUT request", async function () {
-//         productService.putProduct.resolves([
-//             new WebsocketResponse({
-//                 responseId: "ID",
-//                 statusCode: 200,
-//                 statusMessage: "OK",
-//                 headers: {},
-//                 body: {
-//                     "id": 1,
-//                     "name": "TEST_DOC",
-//                     "price": 500,
-//                     "originalPrice": 4000,
-//                     "pictures": ["url1", "url2"],
-//                     "sizeChart": [
-//                         { "key": "chest", "value": "32" },
-//                         { "key": "chest", "value": "32" }
-//                     ]
-//                 }
-//             })
-//         ])
-
-//         const results = await productController.handleRequest(
-//             new InterServiceMessage(
-//                 {
-//                     packet: new WebsocketRequest({
-//                         requestId: "ID",
-//                         method: "PUT",
-//                         url: "https://www.everythng.com/product",
-//                         headers: {},
-//                         body: {
-//                             "name": "TEST_DOC",
-//                             "price": 500,
-//                             "originalPrice": 4000,
-//                             "pictures": ["url1", "url2"],
-//                             "sizeChart": [
-//                                 { "key": "chest", "value": "32" },
-//                                 { "key": "chest", "value": "32" }
-//                             ]
-//                         }
-//                     }),
-//                     uid: "UID",
-//                     socketId: "SOCKET_ID"
-//                 }
-//             )
-//         )
-
-//         assert.deepEqual(results, [
-//             new InterServiceMessage(
-//                 {
-//                     uid: "UID",
-//                     socketId: "SOCKET_ID",
-//                     sendTo: SendTo.SOCKET_ID,
-//                     packet: new WebsocketResponse(
-//                         {
-//                             responseId: "ID",
-//                             statusCode: 200,
-//                             statusMessage: "OK",
-//                             headers: {},
-//                             body: {
-//                                 "id": 1,
-//                                 "name": "TEST_DOC",
-//                                 "price": 500,
-//                                 "originalPrice": 4000,
-//                                 "pictures": ["url1", "url2"],
-//                                 "sizeChart": [
-//                                     { "key": "chest", "value": "32" },
-//                                     { "key": "chest", "value": "32" }
-//                                 ]
-//                             }
-//                         }
-//                     )
-//                 }
-//             )
-//         ])
-//     })
-//     it("should handle DELETE request")
-// })
